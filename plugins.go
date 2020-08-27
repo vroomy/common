0124beb2a0c6f1586ae5e1cd2965b941d70bf423
Derek Halman <d.halman@gmail.com>
2020-08-26 19:26:03 -0700

    move httpserve.Context to vroomy/common interface for plugin shared imports
    gomu: Deploy local changes

--- plugins.go
+++
@@ -1,7 +0,0 @@
-package common
-
-// Plugins represents the plugins library interface. This allows for plugins and libraries
-// to not be affected by vroomy/plugins version changes.
-type Plugins interface {
-	Backend(key string, val interface{}) error
-}
