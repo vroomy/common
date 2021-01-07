f6bef89112ec6234f185b80f458d3ab7ae40cf26
Josh Montoya <josh@montoya.io>
2021-01-06 23:47:36 -0800

    move handler

---
+++ handler.go
@@ -0,0 +1,4 @@
+package common
+
+// Handler is the HTTP handler type
+type Handler func(ctx Context)
--- interfaces.go
+++ interfaces.go
@@ -5,12 +5,3 @@ package common
 type Plugins interface {
 	Backend(key string, val interface{}) error
 }
-
-// Handler is the HTTP handler type
-type Handler func(ctx Context) *Response
-
-// Storage is used as a basic form of Param storage for a Context
-// TODO: Determine with team if it seems valuable to change this to map[string]interface{}.
-// I'd prefer if we can keep it as-is, due to the fact that map[string]string has much less
-// GC overhead. Additionally, avoiding type assertion would be fantastic.
-type Storage map[string]string
